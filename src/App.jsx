5a0dd569ab41eafb3536b4a0dd38d95f02d0f5d3
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-09-01 21:40:48 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Magnetic from "./components/Magnetic"
+
+export default function App() {
+  return (
+    <div className="font-clash">
+      <div className="flex gap-x-4 items-center justify-center h-screen bg-[#f4f0e6] text-[#ff3c00] font-medium text-xl">
+        <a href="">
+          <span>About</span>
+        </a>
+        <a href="">
+          <span>About</span>
+        </a>
+        <a href="">
+          <span>About</span>
+        </a>
+        <a href="">
+          <span>About</span>
+        </a>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Magnetic.jsx
@@ -0,0 +1,7 @@
+export default function Magnetic() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
