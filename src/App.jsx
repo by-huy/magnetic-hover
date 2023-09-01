ab2953c4027e5b1164764e5d962cf2fbe5b6929a
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-09-01 21:46:10 +1000

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@ import Magnetic from "./components/Magnetic"
 
 export default function App() {
   return (
-    <div className="font-clash">
+    <div className="font-clash ">
       <div className="flex gap-x-4 items-center justify-center h-screen bg-[#f4f0e6] text-[#ff3c00] font-medium text-xl">
         <a href="">
           <span>About</span>
         </a>
         <a href="">
-          <span>About</span>
+          <span>Services</span>
         </a>
         <a href="">
-          <span>About</span>
+          <span>Works</span>
         </a>
         <a href="">
-          <span>About</span>
+          <span>Contact</span>
         </a>
+      </div>
+      <div className="absolute bottom-2 left-1/2 -translate-x-1/2 text-[#ff3c00]">
+        <p>built by <a target="_blank" rel="noreferrer" className="hover:font-bold transition-all duration-100" href="https://www.huyng.xyz">HuyNG</a>
+        </p>
       </div>
     </div>
   )
