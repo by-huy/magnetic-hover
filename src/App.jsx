ead4a7d8aeccf3dbf7d5bbde622f023362048557
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-09-01 22:29:49 +1000

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,45 @@ export default function App() {
     <div className="font-clash ">
       <div className="flex gap-x-10 items-center justify-center h-screen bg-[#f4f0e6] text-[#ff3c00] font-medium text-xl">
         <Magnetic>
-          <a href="">
+          <a href="" className="group relative">
             <span>About</span>
+            <div className="absolute rounded-full  group-hover:opacity-100 opacity-0 duration-100 left-1/2 -translate-x-1/2 w-[0.35rem] h-[0.35rem] bg-[#ff3c00]"></div>
           </a>
         </Magnetic>
         <Magnetic>
           <a href="" className="group relative">
             <span>Services</span>
-            <div className="absolute rounded-full  group-hover:opacity-100 opacity-0 duration-100 left-1/2 -translate-x-1/2 w-2 h-2 bg-[#292929]"></div>
+            <div className="absolute rounded-full  group-hover:opacity-100 opacity-0 duration-100 left-1/2 -translate-x-1/2 w-[0.35rem] h-[0.35rem] bg-[#ff3c00]"></div>
           </a>
         </Magnetic>
         <Magnetic>
-          <a href="">
+          <a href="" className="group relative">
             <span>Works</span>
+            <div className="absolute rounded-full  group-hover:opacity-100 opacity-0 duration-100 left-1/2 -translate-x-1/2 w-[0.35rem] h-[0.35rem] bg-[#ff3c00]"></div>
           </a>
         </Magnetic>
         <Magnetic>
-          <a href="">
+          <a href="" className="group relative">
             <span>Contact</span>
+            <div className="absolute rounded-full  group-hover:opacity-100 opacity-0 duration-100 left-1/2 -translate-x-1/2 w-[0.35rem] h-[0.35rem] bg-[#ff3c00]"></div>
           </a>
         </Magnetic>
       </div>
-      <div className="absolute bottom-2 left-1/2 -translate-x-1/2 text-[#ff3c00]">
-        <p>
-          built by{" "}
-          <a
-            target="_blank"
-            rel="noreferrer"
-            className="hover:font-bold transition-all duration-100"
-            href="https://www.huyng.xyz"
-          >
-            HuyNG
-          </a>
-        </p>
-      </div>
+
+
+        <div className="absolute bottom-2 left-1/2 -translate-x-1/2 text-[#ff3c00] flex gap-x-1">
+            <p>built by</p>
+            <Magnetic>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                className="hover:font-bold transition-all duration-100"
+                href="https://www.huyng.xyz"
+              >
+                HuyNG
+              </a>
+            </Magnetic>
+        </div>
     </div>
   );
 }
